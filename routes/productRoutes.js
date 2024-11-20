eb3a6163bb9e0b25f599b4fe3c550f2e83fe97d5
bhavik478 <bhavikptridhyatech@gmail.com>
2024-11-20 17:09:30 +0530

    - delete product api chnages

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,17 +1,18 @@
 const express = require("express");
-const { createProduct, getAllProducts, updateProduct, deleteProduct, getSingleProduct } = require("../controllers/productController");
+const {
+  createProduct,
+  getAllProducts,
+  updateProduct,
+  deleteProduct,
+  getSingleProduct,
+} = require("../controllers/productController");
 const router = express.Router();
-const {protect, restrict} = require("../middleware/authMiddleware")
+const { protect, restrict } = require("../middleware/authMiddleware");
 
+router.post("/product", createProduct);
+router.get("/product", getAllProducts);
+router.put("/product/:id", updateProduct);
+router.get("/product/:id", getSingleProduct);
+router.delete("/product/:id", deleteProduct);
 
-router.post("/product", createProduct)
-router.get("/product", getAllProducts)
-router.put("/product/:id", updateProduct)
-router.get("/product/:id", getSingleProduct)
-router.delete("/product/:id",protect,restrict("admin"), deleteProduct)
-
-
-
-
-
-module.exports = router
+module.exports = router;
